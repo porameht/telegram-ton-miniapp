46cb8b65521e4ea05d23bc806063fb97ab3fe4e3
fr4nk <porameht@outlook.com>
2024-11-20 10:59:12 +0700

    feat: Add configuration script and enhance webapp initialization
    Add a new configure.js script for setting up the bot and improve the webapp initialization process in index.html. The configuration script allows users to input their bot access token and webapp URL, then sets up the bot's chat menu button. The index.html file now includes error handling for Telegram Web App initialization and improves the form submission process.

---
+++ configure.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { createInterface } from "readline";
+import fs from "fs";
+import { promisify } from "util";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question = (question) =>
+  new Promise((resolve) => rl.question(question, resolve));
+
+function exitError(error) {
+  console.error(`Error! ${error}`);
+  process.exit(1);
+}
+
+const banner = `
+████████╗██╗    ██╗ █████╗     ████████╗███████╗███╗   ███╗██████╗ ██╗      █████╗ ████████╗███████╗
+╚══██╔══╝██║    ██║██╔══██╗    ╚══██╔══╝██╔════╝████╗ ████║██╔══██╗██║     ██╔══██╗╚══██╔══╝██╔════╝
+   ██║   ██║ █╗ ██║███████║       ██║   █████╗  ██╔████╔██║██████╔╝██║     ███████║   ██║   █████╗  
+   ██║   ██║███╗██║██╔══██║       ██║   ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██╔══██║   ██║   ██╔══╝  
+   ██║   ╚███╔███╔╝██║  ██║       ██║   ███████╗██║ ╚═╝ ██║██║     ███████╗██║  ██║   ██║   ███████╗
+   ╚═╝    ╚══╝╚══╝ ╚═╝  ╚═╝       ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
+`;
+
+console.log(banner);
+
+let botUsername;
+
+(async () => {
+  const accessToken = await question("Enter your bot access token: ");
+  if (!accessToken?.length > 0) exitError("Token is required");
+
+  const webappUrl = await question("Enter your webapp URL: ");
+  if (!webappUrl?.length > 0) exitError("Webapp URL is required");
+
+  const getBot = await axios.get(
+    `https://api.telegram.org/bot${accessToken}/getMe`
+  ).catch(exitError);
+
+  botUsername = getBot.data.result.username;
+
+  console.log(`\n\nSetting bot ${botUsername} webapp url to ${webappUrl}`);
+
+  const resp = await axios.post(
+    `https://api.telegram.org/bot${accessToken}/setChatMenuButton`,
+    {
+      menu_button: {
+        type: "web_app",
+        text: "Launch Webapp",
+        web_app: {
+          url: webappUrl,
+        },
+      },
+    }
+  ).catch(exitError);
+
+  if (resp.status === 200) {
+    console.log(
+      `\nYou're all set! Visit https://t.me/${botUsername} to interact with your bot`
+    );
+    process.exit();
+  } else {
+    exitError(`\nSomething went wrong! ${resp.error}`);
+  }
+})();
